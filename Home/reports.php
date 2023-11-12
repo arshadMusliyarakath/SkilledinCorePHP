e28f19c91519ba581889f4076a85edbaa9df1682
Arshad Musliyarakath <123569308+arshadMusliyarakath@users.noreply.github.com>
2023-11-12 09:27:39 +0530

    Update reports.php

--- Home/reports.php
+++ Home/reports.php
@@ -34,22 +34,68 @@ include('../Master/header.php');
 					<tr>
 						<th scope="col">#</th>
 						<th scope="col">Project Name</th>
-                        <th scope="col">Task Name</th>
-						<th scope="col">Total Hours</th>
+						<th scope="col" width="20%">Total Hours</th>
 					</tr>
 				</thead>
 				<tbody>
 					<?php
                         $i = 0;
-                        $prjctHours = $model->getAllEntry();
+                        $prjctHours = $model->getProjectHours();
                         foreach ($prjctHours as $key => $prjctHour) {
                             $i++;
                             ?>
                                 <tr>
                                     <th scope="row"><?= $i ?></th>
-                                    <td><?= $prjctHour['project_name'] ?></td>
-                                    <td><?= $prjctHour['task_name'] ?></td>
-                                    <td><?= $prjctHour['hours'] ?></td>
+                                    <td>
+                                    	<div class="accordion" id="accordionExample">
+										  
+										  <div class="accordion-item">
+										    <h2 class="accordion-header">
+										      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse_<?= $i ?>" aria-expanded="false" aria-controls="collapse_<?= $i ?>">
+										        <?= $prjctHour['project_name'] ?>
+										      </button>
+										    </h2>
+										    <div id="collapse_<?= $i ?>" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+										      <div class="accordion-body">
+										        <table class="table table-bordered">
+										        	<thead>
+										        		<tr>
+										        			<th>SL No</th>
+											        		<th>Task Name</th>
+											        		<th>Hours</th>
+										        		</tr>
+										        	</thead>
+										        	<tbody>
+										        		
+										        		<?php
+										        		$j = 0;
+										        			$tasks = $model->getProjectTask($prjctHour['project_id']);
+										        			foreach ($tasks as $key => $value) {
+										        				$j++;
+										        				?>
+
+										        				<tr>
+										        					<td><?= $j ?></td>
+										        					<td><?= $value['task_name'] ?></td>
+										        					<td><?= $value['hours']?></td>
+										        				</tr>
+										        				<?php
+										        			}
+
+										        		?>
+
+
+										        	</tbody>
+										        </table>
+										      </div>
+										    </div>
+										  </div>
+										  
+										</div>
+
+                                    </td>
+              
+                                    <td><?= $prjctHour['total_hours'] ?></td>
                                 </tr>
                             <?php
                         }
